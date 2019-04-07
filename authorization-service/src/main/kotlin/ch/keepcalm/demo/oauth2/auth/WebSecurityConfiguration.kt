f864ffbe8202c8d352af672789a0866fe36dd2c6
Marcel Widmer <marzelwidmer@gmail.com>
2019-04-07 16:40:52 +0200

    remove own delegatingPasswordEncoder impl.

--- authorization-service/src/main/kotlin/ch/keepcalm/demo/oauth2/auth/WebSecurityConfiguration.kt
+++ authorization-service/src/main/kotlin/ch/keepcalm/demo/oauth2/auth/WebSecurityConfiguration.kt
@@ -7,21 +7,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.crypto.password.DelegatingPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
-import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder
-import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder
-import java.util.HashMap
 import javax.sql.DataSource
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers.anyExchange
-import org.springframework.security.authorization.AuthenticatedReactiveAuthorizationManager.authenticated
-import org.springframework.security.config.web.server.ServerHttpSecurity
-import org.springframework.security.web.server.SecurityWebFilterChain
-
-
+import org.springframework.security.crypto.factory.PasswordEncoderFactories
 
 @EnableWebSecurity
 class WebSecurityConfiguration(private val dataSource: DataSource) : WebSecurityConfigurerAdapter() {
@@ -54,27 +44,6 @@ class WebSecurityConfiguration(private val dataSource: DataSource) : WebSecurity
     override fun authenticationManagerBean(): AuthenticationManager = super.authenticationManagerBean()
 
     @Bean
-    fun passwordEncoder(): PasswordEncoder = delegatingPasswordEncoder()
+    fun passwordEncoder(): PasswordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder()
 
-}
-
-private fun delegatingPasswordEncoder(): PasswordEncoder {
-    val idForEncode = "bcrypt"
-    val encoders = HashMap<String, PasswordEncoder>()
-    encoders[idForEncode] = BCryptPasswordEncoder()
-    encoders["pbkdf2"] = Pbkdf2PasswordEncoder()
-    encoders["scrypt"] = SCryptPasswordEncoder()
-
-    // deprecated encoders
-    encoders["ldap"] = org.springframework.security.crypto.password.LdapShaPasswordEncoder()
-    encoders["MD4"] = org.springframework.security.crypto.password.Md4PasswordEncoder()
-    encoders["MD5"] = org.springframework.security.crypto.password.MessageDigestPasswordEncoder("MD5")
-    encoders["noop"] = org.springframework.security.crypto.password.NoOpPasswordEncoder.getInstance()
-    encoders["SHA-1"] = org.springframework.security.crypto.password.MessageDigestPasswordEncoder("SHA-1")
-    encoders["SHA-256"] = org.springframework.security.crypto.password.MessageDigestPasswordEncoder("SHA-256")
-    encoders["sha256"] = org.springframework.security.crypto.password.StandardPasswordEncoder()
-
-    val passwordEncoder = DelegatingPasswordEncoder(idForEncode, encoders)
-    passwordEncoder.setDefaultPasswordEncoderForMatches(BCryptPasswordEncoder(10))
-    return passwordEncoder
 }
